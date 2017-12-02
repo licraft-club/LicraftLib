823258bf01959913aea83e96e0074160095396e5
shell <shellljx@gmail.com>
2017-12-02 17:18:44 +0800

    first commit

---
+++ src/main/java/com/licrafter/lib/LicraftLib.java
@@ -0,0 +1,21 @@
+package com.licrafter.lib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by shell on 2017/12/2.
+ * <p>
+ * Github: https://github.com/shellljx
+ */
+public class LicraftLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
