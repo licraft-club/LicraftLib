d9e4829dadc1a547a278742f50e0540576315afa
shell <shellljx@gmail.com>
2017-12-03 22:27:04 +0800

    feature write default file from jar feature easy log

---
+++ src/main/java/com/licrafter/lib/config/LicraftConfigUtils.java
@@ -0,0 +1,82 @@
+package com.licrafter.lib.config;
+
+import com.licrafter.lib.log.LicraftLog;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * Created by shell on 2017/12/3.
+ * <p>
+ * Github: https://github.com/shellljx
+ */
+public class LicraftConfigUtils {
+
+    /**
+     * write default config file of plugin from jar
+     *
+     * @param plugin
+     */
+    public static void writeDefaultConfigFromJar(JavaPlugin plugin, String configPath) {
+        File dataFolder = plugin.getDataFolder();
+        if (!dataFolder.isDirectory()) {
+            dataFolder.mkdirs();
+        }
+        if (!new File(dataFolder, configPath).isFile()) {
+            if (writeDefaultFileFromJar(plugin, new File(plugin.getDataFolder(), configPath), configPath, true)) {
+                LicraftLog.consoleMessage(plugin, "Wrote default config...");
+            }
+        }
+    }
+
+    /**
+     * write default file from jar
+     *
+     * @param plugin
+     * @param writeName
+     * @param jarPath
+     * @param backupOld
+     * @return
+     */
+    public static boolean writeDefaultFileFromJar(JavaPlugin plugin, File writeName, String jarPath, boolean backupOld) {
+        try {
+            File fileBackup = new File(plugin.getDataFolder(), "backup-" + writeName);
+            File jarloc = new File(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getCanonicalFile();
+            if (jarloc.isFile()) {
+                JarFile jar = new JarFile(jarloc);
+                JarEntry entry = jar.getJarEntry(jarPath);
+                if (entry != null && !entry.isDirectory()) {
+                    InputStream in = jar.getInputStream(entry);
+                    InputStreamReader isr = new InputStreamReader(in, "UTF8");
+                    if (writeName.isFile()) {
+                        if (backupOld) {
+                            if (fileBackup.isFile()) {
+                                fileBackup.delete();
+                            }
+                            writeName.renameTo(fileBackup);
+                        } else {
+                            writeName.delete();
+                        }
+                    }
+                    FileOutputStream out = new FileOutputStream(writeName);
+                    OutputStreamWriter osw = new OutputStreamWriter(out, "UTF8");
+                    char[] tempbytes = new char[512];
+                    int readbytes = isr.read(tempbytes, 0, 512);
+                    while (readbytes > -1) {
+                        osw.write(tempbytes, 0, readbytes);
+                        readbytes = isr.read(tempbytes, 0, 512);
+                    }
+                    osw.close();
+                    isr.close();
+                    return true;
+                }
+            }
+            return false;
+        } catch (Exception ex) {
+            LicraftLog.consoleMessage(plugin, "Failed to write file: " + writeName);
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/licrafter/lib/log/LicraftLog.java
@@ -0,0 +1,38 @@
+package com.licrafter.lib.log;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+/**
+ * Created by shell on 2017/12/3.
+ * <p>
+ * Github: https://github.com/shellljx
+ */
+public class LicraftLog {
+
+
+    public static void printEnableInfo(JavaPlugin plugin) {
+        String enableInfo =
+                "Enabling " + plugin.getName()
+                        + " (Version:" + plugin.getDescription().getVersion()
+                        + " Authors:[" + String.join(",", plugin.getDescription().getAuthors())
+                        + "])"
+                        + " WebSite:" + plugin.getDescription().getWebsite();
+        consoleMessage(plugin, enableInfo);
+    }
+
+    public static void printDisableInfo(JavaPlugin plugin) {
+        String disableInfo = "The plugin " + plugin.getName() + " is Disabled,Thanks for your using!";
+        consoleMessage(plugin, disableInfo);
+    }
+
+    public static void consoleMessage(JavaPlugin plugin, String message) {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', getLogPrefix(plugin) + " " + message));
+    }
+
+    public static String getLogPrefix(JavaPlugin plugin) {
+        return ChatColor.GREEN + "[" + ChatColor.GOLD + plugin.getName() + ChatColor.GREEN + "]" + ChatColor.GRAY;
+    }
+}
